41db10f4a15f4c953284e4716d5fed585c37e0bc
DevPBDias <devpbdias@gmail.com>
2023-10-03 21:43:26 -0300

    feat: added about components and styles

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,6 +1,35 @@
+import './styles.css';
+import aboutBg from '../../assets/aboutBg.png';
+
 function About() {
   return (
-    <div>About</div>
+    <section className="about-section">
+      <img src={ aboutBg } alt="" className="background-about" />
+      <section className="content-about-container">
+        <h2 className="title-about">
+          Fala, galerinha!
+          <br />
+          Treinador Evertinho na área!
+        </h2>
+        <p className="text-about">
+          Aqui é Everton de Paula, conhecido como Treinador Evertinho.
+          Sou ex-atleta profissional de futebol e formado em Educação
+          Física pela Dom Bosco, com algumas pós-graduações.
+          <br />
+          <br />
+          Atualmente, sou preparador físico de atletas de alto rendimento,
+          com treinos focados em ganho de força, resistência, velocidade
+          e agilidade, além de prevenção de lesões. Já trabalhei
+          profissionalmente no São Joseense, equipe do Paraná,
+          e com vários atletas do futebol profissional.
+          <br />
+          <br />
+          Meu trabalho é personalizado e individualizado para o objetivo
+          principal que o atleta escolher, sendo ele em moto remoto ou
+          presencial, da forma como o atleta desejar.
+        </p>
+      </section>
+    </section>
   );
 }
 
