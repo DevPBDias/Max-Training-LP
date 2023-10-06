0cff0dae401cbc94b31236db6db05632f164c0a3
DevPBDias <devpbdias@gmail.com>
2023-10-06 16:16:25 -0300

    feat: added ddepoimentGiuliano to component

--- src/components/depoiments/VideoContainer.tsx
+++ src/components/depoiments/VideoContainer.tsx
@@ -1,11 +1,12 @@
 import Video from './Video';
 import depoimentOne from '../../assets/videos/depoimentOne.mp4';
 import depoimentTwo from '../../assets/videos/depoimentTwo.mp4';
+import depoimentGiuliano from '../../assets/videos/depoimentGiuliano.mp4';
 
 function VideoContainer() {
   return (
     <div className="video-container">
-      <Video video={ depoimentOne } />
+      <Video video={ depoimentGiuliano } />
       <Video video={ depoimentTwo } />
       <Video video={ depoimentOne } />
     </div>
