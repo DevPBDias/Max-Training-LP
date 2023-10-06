d9d7099db72f77f44f932b3b5cbfd810485bc1bf
DevPBDias <devpbdias@gmail.com>
2023-10-06 19:42:11 -0300

    feat: added react router and made style route

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import About from './components/about/About';
-import Contact from './components/contact/Contact';
-import Depoiments from './components/depoiments/Depoiments';
-import Footer from './components/footer/Footer';
-import FrequentQuestions from './components/frequentQuestions/FrequentQuestions';
-import Home from './components/home/Home';
-import Pricing from './components/pricing/Pricing';
-import Reservation from './components/reservation/Reservation';
-import Why from './components/why/Why';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import MaxTraining from './pages/MaxTraining';
 
 export default function App() {
   return (
-    <main>
-      <Home />
-      <Why />
-      <Depoiments />
-      <Reservation />
-      <Pricing />
-      <FrequentQuestions />
-      <Contact />
-      <About />
-      <Footer />
-    </main>
+    <Routes>
+      <Route path="/" element={ <Navigate to="/maxtrainingperformance" /> } />
+      <Route path="/maxtrainingperformance" element={ <MaxTraining /> } />
+    </Routes>
   );
 }
---
+++ src/pages/MaxTraining.tsx
@@ -0,0 +1,25 @@
+import About from '../components/about/About';
+import Contact from '../components/contact/Contact';
+import Depoiments from '../components/depoiments/Depoiments';
+import Footer from '../components/footer/Footer';
+import FrequentQuestions from '../components/frequentQuestions/FrequentQuestions';
+import Home from '../components/home/Home';
+import Pricing from '../components/pricing/Pricing';
+import Reservation from '../components/reservation/Reservation';
+import Why from '../components/why/Why';
+
+export default function App() {
+  return (
+    <main>
+      <Home />
+      <Why />
+      <Depoiments />
+      <Reservation />
+      <Pricing />
+      <FrequentQuestions />
+      <Contact />
+      <About />
+      <Footer />
+    </main>
+  );
+}
