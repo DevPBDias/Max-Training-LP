2d296bbd0388f4cc5bf848926ab33735d1621509
DevPBDias <devpbdias@gmail.com>
2023-10-08 09:59:13 -0300

    fix: styles homebg

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,20 +2,11 @@ import Logo from '../utils/Logo';
 import Bonus from './Bonus';
 import BuyBtn from '../utils/BuyBtn';
 import ContentText from './ContentText';
-import homeBg from '../../assets/bg-desktop/homeBg.webp';
-import homeBgMobile from '../../assets/bg-mobile/homeBgMobile.webp';
 import './styles.css';
 
 function Home() {
   return (
     <section className="home-section">
-      <picture className="background-container">
-        <source
-          srcSet={ homeBg }
-          media="(min-width: 1200px)"
-        />
-        <img src={ homeBgMobile } alt="" className="background-home" />
-      </picture>
       <section className="content-home-container">
         <Logo />
         <ContentText />
