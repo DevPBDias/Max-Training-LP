c865257bae43d587e2fa123888f02a83c989caf6
DevPBDias <devpbdias@gmail.com>
2023-10-11 08:53:03 -0300

    fix: added videos to channel youtube not listed to test solution for safari browser problem

--- src/components/depoiments/VideoContainer.tsx
+++ src/components/depoiments/VideoContainer.tsx
@@ -1,7 +1,4 @@
 import Video from './Video';
-import depoimentOne from '../../assets/videos/videoTwo.mp4';
-import depoimentTwo from '../../assets/videos/videoOne.mp4';
-import depoimentGiuliano from '../../assets/videos/videoGiu.mp4';
 import thumbnailGiu from '../../assets/thumbGiu.webp';
 import thumbnailTwo from '../../assets/thumbTwo.webp';
 import thumbnailOne from '../../assets/thumbOne.webp';
@@ -11,15 +8,15 @@ function VideoContainer() {
     <div className="video-container">
       <Video
         poster={ thumbnailGiu }
-        video={ depoimentGiuliano }
+        video="https://youtube.com/shorts/N59a1Epauvc?feature=share"
       />
       <Video
         poster={ thumbnailOne }
-        video={ depoimentTwo }
+        video="https://youtube.com/shorts/bYMA_Bi58XQ"
       />
       <Video
         poster={ thumbnailTwo }
-        video={ depoimentOne }
+        video="https://youtube.com/shorts/MIUKD66kZ1E"
       />
     </div>
   );
