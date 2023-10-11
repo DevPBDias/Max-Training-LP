2c5c04c18432a300fbc406d7950358880df3af26
DevPBDias <devpbdias@gmail.com>
2023-10-11 08:41:18 -0300

    fix: try another solution for video browser safari

--- src/components/depoiments/Video.tsx
+++ src/components/depoiments/Video.tsx
@@ -5,7 +5,8 @@ type IVideoProps = {
 
 function Video({ video, poster }: IVideoProps) {
   return (
-    <video poster={ poster } className="video" controls src={ video }>
+    <video poster={ poster } className="video" controls>
+      <source src={ video } type="video/mp4" />
       <track default kind="captions" srcLang="pt" />
     </video>
   );
